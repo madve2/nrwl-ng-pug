0322d0ed28690f01ca020fad0cda3fa2b7208ce5
Simon McEnlly <simon.mcenlly@gmail.com>
2020-05-15 09:33:48 +1000

    Update jest configuration to allow Wallaby to run from the project root

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,24 @@
+const path = require('path');
+const isNgCommand =
+  process.argv
+    .map(arg => arg.split(path.sep))
+    .filter(argParts => argParts[argParts.length - 1] === 'ng').length !== 0;
+
 module.exports = {
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   transform: {
     '^.+\\.(ts|js|html)$': 'ts-jest',
-    '^.+\\.(pug)$': `${__dirname}/tools/pug-jest`,
+    '^.+\\.(pug)$': `${__dirname}/tools/pug-jest`
   },
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html']
+  coverageReporters: ['html'],
+  ...(isNgCommand
+    ? {}
+    : {
+        projects: [
+          '<rootDir>/libs/ng-pug-sample-lib',
+          '<rootDir>/apps/ng-pug-sample-app'
+        ]
+      })
 };
-module.exports.setupFilesAfterEnv = ['./src/test-setup.ts'];
